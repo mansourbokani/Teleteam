f196d2dadc7b7bc41623640e256f540a601b2f32
mansourbokani <jamalbokani93@gmail.com>
2016-03-15 21:35:44 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -225,8 +225,9 @@ function create_config( )
     "all",
     "leave_ban",
     "admin"
-    },
-    sudo_users = {110626080,103649648,143723991,111020322,0,tonumber(our_id)},--Sudo users
+
+    sudo_users = {@MANSOUR_M2N}
+tonumber(our_id)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
